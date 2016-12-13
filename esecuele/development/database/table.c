6d2e7ef118ce585f1a3b2f1fd786f6a8802c6c80
Carlos Ignacio Isasa Martin <carlosisamar@gmail.com>
2016-12-13 19:55:17 +0100

    table

--- esecuele/development/database/table.c
+++ esecuele/development/database/table.c
@@ -5,45 +5,33 @@
 #include "../type/type.h"
 
 struct table_ {
-	FILE* f;
+	long *f;
 	int ncols;
 	long first_pos;
 	long last_pos;
-	type_t* types;
+	
 };
 
 void table_create(char* path, int ncols, type_t* types) {
-	table_t* table = NULL; 	
-
-	if(!path || ncols <= 0 || !types) {
-		printf("Error en la entrada de table_create");
-		return;
-	}
-
-	table = (table_t*)malloc(sizeof(table_t))
-	if(!table) return;
-
-	table->f = path;
-	table->ncols = ncols
-	table->first_pos = 0;
-	table->last_pos = 0;
-	table->types = types;
-
+	int i;
+	FILE *f;
+	f = fopen (path, "w");
+	fwrite(ncols, sizeof(int), 1, f);
+	fwrite(types, sizeof(int), ncols, f);
+	fclose(f);
 	return;
 }
 
 table_t* table_open(char* path) {
-	
+	table_t *t;
+	FILE *f;
+	t = (table_t*) malloc (sizeof(table_t));
+	f = fopen(path, "r");
+
 }
 
 void table_close(table_t* table) {
-	if(table != NULL) {
-		/*Eliminacion de registros*/		
 
-		fclose(table->f);
-		free(table);
-	}
-	return;
 }
 
 int table_ncols(table_t* table) {
@@ -59,15 +47,11 @@ type_t* table_types(table_t* table) {
 }
 
 long table_first_pos(table_t* table) {
-	if(!table) return -1;
 
-	return table->first_pos;
 }
 
 long table_last_pos(table_t* table) {
-	  if(!table) return -1;
 
-	return table->last_pos;
 }
 
 record_t* table_read_record(table_t* table, long pos) {
